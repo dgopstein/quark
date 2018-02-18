f35c27ff53d359aa913c5ce27406ca954d21653b
Dan Gopstein <dan@gopstein.com>
2018-02-17 20:01:57 -0500

    linux

--- gensim_doc2vec.py
+++ gensim_doc2vec.py
@@ -8,11 +8,15 @@ from gensim.models import Doc2Vec
 import gensim.models.doc2vec
 import multiprocessing
 from collections import namedtuple, Counter
-from find_files import filetypes_in_dirs
+from find_files import filetypes_in_dirs, c_files_in_dirs
 import re
 
+# https://github.com/statsmodels/statsmodels/issues/3931
+from scipy import stats
+stats.chisqprob = lambda chisq, df: stats.chi2.sf(chisq, df)
+
 def tokenize_source(s):
-    token_pat = '(?:[][.,;{}()]|(?: +))'
+    token_pat = '(?:[][.,;{}()|+=*&^%!~-]|\\.|(?: +))'
     return re.findall(re.compile(token_pat + '|(?:(?!' + token_pat + ').)+'), s)
 
 CodeDocument = namedtuple('CodeDocument', 'words split tags directory')
@@ -25,19 +29,25 @@ CodeDocument = namedtuple('CodeDocument', 'words split tags directory')
 #     print(tokenize_source(file_obj.read())[-50:])
 
 def load_docs():
-    java_files = filetypes_in_dirs(["java"], ["~/opt/src/tomcat85"])
+    #files = filetypes_in_dirs(["java"], ["~/opt/src/tomcat85"])
+    files = c_files_in_dirs(["~/opt/src/linux"])
     doc_idx = 0
     alldocs = []
-    for filename in java_files:
+    #[x for x in files if re.match(r'.*/linux/.*/linux/', x)][:3]
+    for filename in files: #[:100]:
         with open(filename, 'r') as file_obj:
-            file_content = tokenize_source(file_obj.read())
-            split = ['train', 'test', 'validate', 'extra'][doc_idx % 4]
-            tags = [filename]
-            #directory = re.match(r'.*(tomcat85/.*/)[^/]*', filename).group(1)
-            #directory = re.match(r'.*tomcat85/([^/]*(?:/[^/]*)?).*', filename).group(1)
-            directory = re.match(r'.*tomcat85/([^/]*).*', filename).group(1)
-            doc_idx += 1
-            alldocs.append(CodeDocument(file_content, split, tags, directory))
+            try:
+                file_content = tokenize_source(file_obj.read())
+                #split = ['train', 'test', 'validate', 'extra'][doc_idx % 4]
+                split = ['train', 'train', 'train', 'test'][doc_idx % 4]
+                tags = [filename]
+                #directory = re.match(r'.*(tomcat85/.*/)[^/]*', filename).group(1)
+                #directory = re.match(r'.*tomcat85/([^/]*(?:/[^/]*)?).*', filename).group(1)
+                directory = re.match(r'.*?/linux/([^/]*).*', filename).group(1)
+                doc_idx += 1
+                alldocs.append(CodeDocument(file_content, split, tags, directory))
+            except:
+                pass
 
     train_docs = [doc for doc in alldocs if doc.split == 'train']
     test_docs = [doc for doc in alldocs if doc.split == 'test']
@@ -45,9 +55,9 @@ def load_docs():
     return (alldocs, train_docs, test_docs)
 
 alldocs, train_docs, test_docs = load_docs()
-
-classes = list(Counter([x.directory for x in train_docs]).keys())
 doc_list = alldocs[:]  # For reshuffling per pass
+
+id2class = sorted(list(Counter([x.directory for x in train_docs]).keys()))
 
 ###################################################
 # Create the models
@@ -81,12 +91,6 @@ def elapsed_timer():
     end = default_timer()
     elapser = lambda: end-start
 
-def logistic_predictor_from_data(train_targets, train_regressors):
-    logit = sm.MNLogit(train_targets, train_regressors)
-    predictor = logit.fit(disp=0)
-    # print(predictor.summary())
-    return predictor
-
 # test_model = pv_dm
 # train_set = train_docs
 # test_set = test_docs
@@ -94,12 +98,19 @@ def error_rate_for_model(test_model, train_set, test_set, infer=False, infer_ste
     """Report error rate on test_doc sentiments, using supplied model and train_docs"""
 
     train_targets, train_regressors = zip(*[(doc.directory, test_model.docvecs[doc.tags[0]]) for doc in train_set])
-    #train_regressors = sm.add_constant(train_regressors)
-    train_targets[0]
+    train_regressors = sm.add_constant(train_regressors)
 
-    with elapsed_timer() as elapsed:
-        predictor = logistic_predictor_from_data(train_targets, train_regressors)
-        #print("elapsed time", elapsed())
+    # logit2 = sm.MNLogit(["a", "b", "c", "a", "b", "c"], [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
+    # predictor2 = logit2.fit(method='bfgs', disp=1)
+    # np.rint(predictor2.predict([[.8, .1, .1, 0], [.2, .6, .2, 0], [.1, .2, .7, 0]]))
+    # predictor2.summary()
+
+    #Counter(train_target_names)
+    logit = sm.MNLogit(train_targets, train_regressors)
+    id2class = logit._ynames_map
+    predictor = logit.fit(method='bfgs', disp=0)
+    # predictor.summary()
+    #predictor.mle_retvals
 
     test_data = test_set
     if infer:
@@ -108,16 +119,16 @@ def error_rate_for_model(test_model, train_set, test_set, infer=False, infer_ste
         test_regressors = [test_model.infer_vector(doc.words, steps=infer_steps, alpha=infer_alpha) for doc in test_data]
     else:
         test_regressors = [test_model.docvecs[doc.tags[0]] for doc in test_docs]
-    #test_regressors = sm.add_constant(test_regressors)
+    test_regressors = sm.add_constant(test_regressors)
 
     # Predict & evaluate
-    test_regressors[0]
+    #test_regressors[0]
     with elapsed_timer() as elapsed:
         test_predictions = predictor.predict(test_regressors)
         #print("elapsed time", elapsed())
-    predictor
-    [doc.directory for doc in test_data]
-    predicted_classes = [classes[x] for x in np.argmax(test_predictions, axis=1)]
+    #predictor
+    #[doc.directory for doc in test_data]
+    predicted_classes = [id2class[x] for x in np.argmax(test_predictions, axis=1)]
     correct_classes = [x == y for (x, y) in zip(predicted_classes, [doc.directory for doc in test_data])]
     corrects = Counter(correct_classes)[True]
     errors = len(test_predictions) - corrects
@@ -128,15 +139,29 @@ def error_rate_for_model(test_model, train_set, test_set, infer=False, infer_ste
 # Training
 ###################################################
 
+
 from collections import defaultdict
 
 from random import shuffle
 import datetime
 
 def train_pvdm():
-    alpha, min_alpha, passes = (0.025, 0.001, 200)
+    def eval_error(name, infer):
+        eval_duration = ''
+        with elapsed_timer() as eval_elapsed:
+            err, err_count, test_count, predictor = error_rate_for_model(pv_dm, train_docs[::100], test_docs[::100], infer=infer)
+            eval_duration = '%.1f' % eval_elapsed()
+        best_indicator = ' '
+        if err < best_error[name]:
+            best_error[name] = err
+            best_indicator = '*'
+        all_errors[name].append(err)
+        print("%s%f : %i passes : %s %ss %ss" % (best_indicator, err, epoch + 1, name, duration, eval_duration))
+
+    alpha, min_alpha, passes = (0.1, 0.001, 500)
     alpha_delta = (alpha - min_alpha) / passes
     best_error = defaultdict(lambda: 1.0)  # To selectively print only best errors achieved
+    all_errors = defaultdict(lambda: [])  # To selectively print only best errors achieved
     name = "pvdm"
 
     start_time = datetime.datetime.now()
@@ -151,35 +176,24 @@ def train_pvdm():
             duration = '%.1f' % elapsed()
 
         ## Evaluate
-        eval_duration = ''
-        with elapsed_timer() as eval_elapsed:
-            err, err_count, test_count, predictor = error_rate_for_model(pv_dm, train_docs, test_docs)
-        eval_duration = '%.1f' % eval_elapsed()
-        best_indicator = ' '
-        if err <= best_error[name]:
-            best_error[name] = err
-            best_indicator = '*'
-        print("%s%f : %i passes : %s %ss %ss" % (best_indicator, err, epoch + 1, name, duration, eval_duration))
+        if ((epoch + 1) % 1) == 0 or epoch == 0:
+            eval_error(name, infer=False)
 
-        if ((epoch + 1) % 5) == 0 or epoch == 0:
-            eval_duration = ''
-            with elapsed_timer() as eval_elapsed:
-                infer_err, err_count, test_count, predictor = error_rate_for_model(pv_dm, train_docs, test_docs, infer=True)
-            eval_duration = '%.1f' % eval_elapsed()
-            best_indicator = ' '
-            if infer_err < best_error[name + '_inferred']:
-                best_error[name + '_inferred'] = infer_err
-                best_indicator = '*'
-            print("%s%f : %i passes : %s %ss %ss" % (best_indicator, infer_err, epoch + 1, name + '_inferred', duration, eval_duration))
+        #if ((epoch + 1) % 50) == 0 or epoch == 0:
+        #    eval_error(name + '_inferred', infer=True)
 
         #print('Completed pass %i at alpha %f' % (epoch + 1, alpha))
         alpha -= alpha_delta
 
     end_time = datetime.datetime.now()
+    print("all_errors: ", all_errors)
     print("DURATION %s" % (end_time-start_time))
 
 train_pvdm()
 
-pv_dm.most_similar("char")
+pv_dm.most_similar("protected")
 pv_dm.wv.most_similar(positive=['boolean', 'int'], negative=['Integer'])
 list(pv_dm.wv.vocab.keys())[:100]
+
+#import gnuplotlib as gp
+#gp.plot( [1, 2, 3, 2, 1] )
