03f1f3015dc72c9eb00fb9cb24dbbee8f3f336f8
Dan Gopstein <dan@gopstein.com>
2018-02-21 00:00:33 -0500

    Predict confusingness (quite well) with linear regression

--- linear_confusion_predictor.py
+++ linear_confusion_predictor.py
@@ -5,22 +5,29 @@ import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-import pandas
+import pandas as pd
 
 pv_dm = Doc2Vec.load('tmp/linux_2018-02-20T18:32:02.694306_model_best.pkl')
 
-sources = ["int main() {\n  int x = 2; x += 3; }", "int main() {\n  int x = 4; x += 5; }", "int main() {\n  int y = 2; y += 3; }"]
+snippet_results = pd.read_csv('tmp/results_normalized.csv')
+snippet_questions = pd.read_csv('tmp/questions.csv')
 
-vectors = [pv_dm.infer_vector(tokenize_source(source), steps=3, alpha=0.1) for source in sources]
+vectors = [pv_dm.infer_vector(tokenize_source(source), steps=3, alpha=0.1) for source in snippet_questions['Code']]
 
-snippet_results = pandas.read_csv('tmp/results_normalized.csv')
-
-snippet_results.columns
+tfs = snippet_results.groupby('CodeID')['Correct'].value_counts().unstack().fillna(0)
+scores = tfs['T'] / (tfs['T'] + tfs['F'])
 
 regr = linear_model.LinearRegression()
 
+train_vectors = vectors[::2]
+test_vectors = vectors[1::2]
+
+train_scores = scores[::2]
+test_scores = scores[1::2]
+
 # Train the model using the training sets
-regr.fit(vectors, [1, 2, 3])
+regr.fit(train_vectors, train_scores)
 
 # Make predictions using the testing set
-regr.predict([vectors[2]])
+mean_squared_error(train_scores, regr.predict(train_vectors))
+mean_squared_error(test_scores, regr.predict(test_vectors))
