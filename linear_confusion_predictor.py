0945f0c0ecae0e5ecf5fdb6463d6d9727536f139
Dan Gopstein <dan@gopstein.com>
2018-02-21 23:33:49 -0500

    measure by correlation

--- linear_confusion_predictor.py
+++ linear_confusion_predictor.py
@@ -4,7 +4,7 @@ import statsmodels as sm
 from ggplot import *
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets, linear_model
+import sklearn as skl
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from gensim.models import Doc2Vec
@@ -12,7 +12,7 @@ from gensim_doc2vec import tokenize_source
 
 import pandas as pd
 
-pv_dm = Doc2Vec.load('tmp/linux_2018-02-20T18:32:02.694306_model_best.pkl')
+pv_dm = Doc2Vec.load('tmp/linux_2018-02-21T15:57:56.584064_model_best.pkl')
 
 snippet_results = pd.read_csv('tmp/results_normalized.csv')
 snippet_questions = pd.read_csv('tmp/questions.csv')
@@ -26,7 +26,7 @@ scores = tfs['T'] / (tfs['T'] + tfs['F'])
 tfs = snippet_results.groupby('CodeID')['Correct'].value_counts().unstack().fillna(0)
 tfs
 
-train_vectors, test_vectors, train_scores, test_scores = train_test_split(vectors, scores, test_size=0.15, random_state=1 )
+train_vectors, test_vectors, train_scores, test_scores = train_test_split(vectors, scores, test_size=0.15, random_state=2 )
 
 #alphas = []
 #alpha_error = []
@@ -41,23 +41,23 @@ train_vectors, test_vectors, train_scores, test_scores = train_test_split(vector
 #plt.plot(alphas, alpha_error)
 #plt.show()
 
-# Ridge + Regularization
-sm_res = regr = linear_model.Ridge(alpha=0.001)
-regr.fit(train_vectors, train_scores)
-
 # GLM - Binomial/Logit
 # sm_regr = sm.genmod.generalized_linear_model.GLM(train_scores, train_vectors, family=sm.genmod.families.family.Binomial(link=sm.genmod.families.links.logit))
 # sm_res = sm_regr.fit_regularized(alpha=0.0000001)
 
 # Lasso + Regularization
-# sm_res = regr = linear_model.Lasso(alpha=0)
+# sm_res = regr = skl.linear_model.Lasso(alpha=0)
 # regr.fit(train_vectors, train_scores)
 
-train_predicted = np.minimum(1, sm_res.predict(train_vectors))
-test_predicted  = np.minimum(1, sm_res.predict(test_vectors))
+# Ridge + Regularization
+sm_res = regr = skl.linear_model.Ridge(alpha=20000)
+regr.fit(train_vectors, train_scores)
 
-print("train: ", mean_squared_error(train_scores, train_predicted))
-print("test: ", mean_squared_error(test_scores, test_predicted))
+train_predicted = np.minimum(10, sm_res.predict(train_vectors))
+test_predicted  = np.minimum(10, sm_res.predict(test_vectors))
+
+print("train: ", np.corrcoef(train_scores, sm_res.predict(train_vectors))[0][1])
+print("test: ", np.corrcoef(test_scores, sm_res.predict(test_vectors))[0][1])
 
 predicted_confusion = pd.concat([
     pd.DataFrame.from_records({"observed": train_scores, "predicted": train_predicted, "inferred": False}),
@@ -68,5 +68,3 @@ ggplot(aes(x='observed', y='predicted', color='inferred'), data=predicted_confus
     geom_point() +\
     scale_color_brewer(type='qual', palette=2)
 
-np.corrcoef(train_scores, sm_res.predict(train_vectors))[0][1]
-np.corrcoef(test_scores, sm_res.predict(test_vectors))[0][1]
