a25d6a634c42b8c0d99f02f777bbca571fc35bbc
Dan Gopstein <dan@gopstein.com>
2018-04-19 14:17:50 -0400

    utility functions

--- lex/python/main.py
+++ lex/python/main.py
@@ -1,6 +1,10 @@
 import sys
 sys.path.insert(0, '../grammars-v4/java/')
 
+import collections
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+
 from antlr4 import *
 from antlr4.tree.Tree import TerminalNodeImpl
 from JavaLexer import JavaLexer
@@ -11,40 +15,68 @@ class PrintListener(JavaParserListener):
     def enterCompilationUnit(self, cu):
         print("getChildren: %s" % list(cu.getChildren()))
 
-    #def visitTerminal(self, node):
-    #    print("getSymbol: %s" % node.getSymbol())
-
-    #def enterEveryRule(self, ctx):
-    #    #print("Dir ctx: %s" % dir(ctx))
-    #    print("getText: %s" % ctx.getText())
-    #    #print("getToken: %s" % ctx.getToken())
-    #    #print("getTokens: %s" % ctx.getTokens())
-    #    print("getPayload: %s" % ctx.getPayload())
-    #    #print("toString: %s" % ctx.toString())
-    #    print("toStringTree: %s" % ctx.toStringTree())
-    #    #print("Enter EVERY rule: %s" % ctx.ID())
-
-def toTree(node):
+def to_tree(node):
     ret = None
     if node.getChildCount() > 0:
-        ret = [toTree(child) for child in node.getChildren()]
+        ret = [to_tree(child) for child in node.getChildren()]
     else:
         ret = []
     ret.insert(0, node)
     return ret
 
-def main(argv):
-    input = FileStream("../java/HelloWalker.java")
-
-    #input = "hello abc"
+def java_file_to_tree(filename):
+    input = FileStream(filename)
     lexer = JavaLexer(input)
     stream = CommonTokenStream(lexer)
     parser = JavaParser(stream)
     tree = parser.compilationUnit()
-    return toTree(tree)
-    #printer = PrintListener()
-    #walker = ParseTreeWalker()
-    #walker.walk(printer, tree)
+    return to_tree(tree)
+
+def head_children(lst):
+    head = lst[0]
+    children = lst[1:]
+    return (head, children)
+
+def leaves(tree):
+    head, children = head_children(tree)
+    if children:
+        return [child_leaf for child in children for child_leaf in leaves(child)]
+    else:
+        return [head]
+
+def map_tree(fn, tree):
+    head, children = head_children(tree)
+    ret = [map_tree(fn, child) for child in children]
+    ret.insert(0, fn(head))
+    return ret
+
+def flatten_prefix(tree):
+    lst = []
+    for item in tree:
+        if isinstance(item, collections.Sequence):
+            lst += flatten_prefix(item)
+        else:
+            lst += [item]
+    return lst
+
+tree = java_file_to_tree("../java/HelloWalker.java")
+root = tree[0]
+flat_tree_prefix = flatten_prefix(tree)
+leaf = leaves(tree)[0]
+
+map_tree(lambda n: n.getText(), [leaf])
+
+map_tree(lambda n: n.getText(), tree)
+
+root.getText()
+root.qualifiedName()
+dir(root)
+
+pp.pprint(tree)
+pp.pprint(flat_tree_prefix)
+
+[print(node) for node in flat_tree_prefix]
+[print(node) for node in leaves(tree)]
 
 if __name__ == '__main__':
-    print(main(sys.argv))
+    print(leaf)
